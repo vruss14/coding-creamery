46a43c24372bbcf870eb56c9dc9d677aba9d2265
Valerie Russell <vruss14@gmail.com>
2021-09-08 19:16:41 -0400

    added dynamically generated sprinkles & main container

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+function createSprinkles() {
+
+    let container = document.getElementById("sprinkle-container");
+    let color = ["red", "orange", "yellow", "green", "blue", "purple", "pink"];
+    let rotation = ["negative-fifty", "negative-forty", "negative-thirty", "negative-twenty", "negative-ten", "ten", "twenty", "thirty", "forty", "fifty"];
+
+    for(let i=0; i<300; i++) {
+
+        let sprinkle = document.createElement("div");
+        sprinkle.classList.add("sprinkle");
+
+        let colorClass = color[Math.floor(Math.random() * color.length)];
+        sprinkle.classList.add(`${colorClass}`);
+
+        let rotationClass = rotation[Math.floor(Math.random() * rotation.length)];
+        sprinkle.classList.add(`${rotationClass}`);
+
+        container.appendChild(sprinkle);
+    }
+}
+
+createSprinkles();
