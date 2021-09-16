b64676b98f23abeadd36e628f76d771713583a8e
Valerie Russell <vruss14@gmail.com>
2021-09-16 14:06:06 -0400

    adjusted sprinkle function iterations based on viewport width

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,51 @@ function createSprinkles() {
     let color = ["red", "orange", "yellow", "green", "blue", "purple", "pink"];
     let rotation = ["negative-fifty", "negative-forty", "negative-thirty", "negative-twenty", "negative-ten", "ten", "twenty", "thirty", "forty", "fifty"];
 
-    for(let i=0; i<1200; i++) {
+    // Number of sprinkles (loop iterations) dependent on viewport width
 
-        let sprinkle = document.createElement("div");
-        sprinkle.classList.add("sprinkle");
+    if (window.innerWidth > 1024) {
+        for(let i=0; i<1100; i++) {
 
-        let colorClass = color[Math.floor(Math.random() * color.length)];
-        sprinkle.classList.add(`${colorClass}`);
+            let sprinkle = document.createElement("div");
+            sprinkle.classList.add("sprinkle");
+    
+            let colorClass = color[Math.floor(Math.random() * color.length)];
+            sprinkle.classList.add(`${colorClass}`);
+    
+            let rotationClass = rotation[Math.floor(Math.random() * rotation.length)];
+            sprinkle.classList.add(`${rotationClass}`);
+    
+            container.appendChild(sprinkle);
+        }
+    } else if(window.innerWidth > 768) {
+        for(let i=0; i<1430; i++) {
 
-        let rotationClass = rotation[Math.floor(Math.random() * rotation.length)];
-        sprinkle.classList.add(`${rotationClass}`);
+            let sprinkle = document.createElement("div");
+            sprinkle.classList.add("sprinkle");
+    
+            let colorClass = color[Math.floor(Math.random() * color.length)];
+            sprinkle.classList.add(`${colorClass}`);
+    
+            let rotationClass = rotation[Math.floor(Math.random() * rotation.length)];
+            sprinkle.classList.add(`${rotationClass}`);
+    
+            container.appendChild(sprinkle);
+        }
+    } else {
+        for(let i=0; i<800; i++) {
 
-        container.appendChild(sprinkle);
-    }
+            let sprinkle = document.createElement("div");
+            sprinkle.classList.add("sprinkle");
+    
+            let colorClass = color[Math.floor(Math.random() * color.length)];
+            sprinkle.classList.add(`${colorClass}`);
+    
+            let rotationClass = rotation[Math.floor(Math.random() * rotation.length)];
+            sprinkle.classList.add(`${rotationClass}`);
+    
+            container.appendChild(sprinkle);
+        }
+    } 
 }
 
 createSprinkles();
